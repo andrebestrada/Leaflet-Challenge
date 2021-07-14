cc10aef5d9a4fedaf911541ca65b6427361353cd
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-07-14 12:02:38 -0500

    finish hw

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -75,27 +75,61 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
           fillColor: fillColor(feature.properties.mag),
           color: '#000000',
           fillOpacity: 1,
-          weight: 0.6
-          
+          weight: 0.6 
         })
+      },
+
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
       }
       
+    }).addTo(earthquakes);
+
+    earthquakes.addTo(myMap);
+
+    // Add legend 
+    var legend = L.control({
+      position: "bottomright"
+    });
+  
+    legend.onAdd = function() {
+      var div = L
+        .DomUtil
+        .create("div", "info legend");
+  
+      var grades = [0, 1, 2, 3, 4, 5];
+      var colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+  
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+  
+    legend.addTo(myMap);
+
     
-      }).addTo(myMap);
 
 
-})
+
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(data=>{
+    L.geoJson(data,{
+      color: "blue",
+      weight: 2
+    }).addTo(tectonicplates)
+
+    tectonicplates.addTo(myMap)
+
+  });
+});
 
 
-// Loop through the countries array
-countries.forEach((country)=>{
 
-  L.circleMarker(country.location,{
-    color:"white",
-    fillColor: "blue",
-    fillOpacity:0.7,
-    radius: country.points / 8
-  }).bindPopup(`<h1>${country.name}</h1> <hr> Points: ${country.points}`)
-  .addTo(myMap);
-
-})
